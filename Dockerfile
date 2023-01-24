91b93e67e2149e6b59d15cd818501e7cd86388e0
veoDoble <gustavoguerrerom@gmail.com>
2023-01-23 23:38:37 -0300

    avanza login

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM php:7.4-apache 
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 ENV APACHE_DOCUMENT_ROOT=/var/www/html/public
 RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
 RUN sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
--- frontend/public/login.js
+++ frontend/public/login.js
@@ -1,37 +1,35 @@
 import { API_LOGIN_URL } from "./config.js";
+
 const formLogin = document.querySelector('#formLogin');
+
 
 
 formLogin.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const datosForm = new FormData(formLogin);
+    const email = document.getElementById('email').value;
+    localStorage.setItem('usuario', email );
 
     /*let request = */fetch(API_LOGIN_URL, {
         method: "POST",
         body: datosForm
     })
     .then(resp => resp.text())
-    .then(data => {
-        console.log(data);
-    })
-})
+    .then(data => {const val = JSON.parse(data);
+    if (val.Resultado === true){
+        console.log(val.Resultado);
+        //sessionStorage.getItem('usuario');
+        //document.querySelector('.mensaje').innerHTML = val.Mensaje;
+        window.location.replace("inicio.html");
+        
 
-
-/*
-window.onload = iniciar;
-
-function iniciar(){
-    let btnAgregar = document.getElementById("btnAgregar");
-    btnAgregar.addEventListener("click", clickBtnAgregar)
-}
-
-function clickBtnAgregar() {
-    let txtNota = document.getElementById("txtNota");
-
-    localStorage.nota = txtNota.value;
-    let divNotas = document.getElementById("divNotas");
-    divNotas.innerHTML = txtNota.value;
+    } else{
+        
+        //alert(val.Mensaje);
+        document.querySelector('.mensaje').innerHTML = val.Mensaje
+    }
     
-}
-*/
+    })
+    
+})
