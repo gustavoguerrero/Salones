234c9f6a5341e4bb678585eaab995a6b15c151c0
veoDoble <gustavoguerrerom@gmail.com>
2023-06-23 20:11:18 -0300

    commit

--- frontend/public/lclStorage.js
+++ frontend/public/lclStorage.js
@@ -1,2 +1,12 @@
 const lclStorage = localStorage.getItem('usuario'); 
 if (localStorage.getItem('usuario')) window.location.replace("inicio.html");
+
+
+const session = document.querySelector("#session");
+console.log(session);
+
+function cerrarSession(session) {
+    
+    session.localStorage.removeItem('usuario');
+    window.location.replace("/");
+}
