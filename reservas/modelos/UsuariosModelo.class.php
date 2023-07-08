e41d305a5ae16ebe915245bed41226c15693382c
veodoble <gustavoguerrerom@gmail.com>
2023-07-08 10:08:17 -0300

    refactor usuarioModelo

--- reservas/modelos/UsuariosModelo.class.php
+++ reservas/modelos/UsuariosModelo.class.php
@@ -3,28 +3,26 @@
     require_once "../utils/autoload.php";
 
     class UsuariosModelo extends Modelo{
-        public $idUsuario;
-        public $nombres;
-        public $apellidos;
+        public $id;
+        public $nombre;
         public $email;
 
-        public function __construct($idUsuario=""){
+        public function __construct($id=""){
             parent::__construct();
-            if($idUsuario != ""){
-                $this -> idUsuario = $idUsuario;
+            if($id != ""){
+                $this -> id = $id;
                 $this -> Obtener();
             }
         }
 
         public function Guardar(){
-            if($this -> idUsuario == NULL) $this -> insertar();
+            if($this -> id == NULL) $this -> insertar();
             else $this -> actualizar();
         }
 
         private function insertar(){
-            $sql = "INSERT INTO Usuarios (nombres, apellidos, email) 
-            VALUES ('" . $this -> nombres . "',
-                    '" . $this -> apellidos . "',
+            $sql = "INSERT INTO Usuarios (nombre, email) 
+            VALUES ('" . $this -> nombre . "',
                     '" . $this -> email . "');"; 
             
             $this -> conexion -> query($sql);
@@ -32,29 +30,27 @@
 
         private function actualizar(){
             $sql = "UPDATE Usuarios SET
-            nombres = '" . $this -> nombres . "',
-            apellidos = '" . $this -> apellidos . "',
+            nombre = '" . $this -> nombre . "',
             email = '" . $this -> email . "',
-            WHERE idUsuario = " . $this -> idUsuario . ";";
+            WHERE id = " . $this -> id . ";";
             $this -> conexion -> query($sql);   
         }
 
         public function Obtener(){
             $sql = "SELECT * FROM Usuarios WHERE
-                idUsuario = " . $this -> idUsuario . ";";
+                id = " . $this -> id . ";";
 
             $fila = $this -> conexion -> query($sql) -> fetch_all(MYSQLI_ASSOC)[0];
 
-            $this -> idUsuario = $fila['idUsuario'];
-            $this -> nombres = $fila['nombres'];
-            $this -> apellidos = $fila['apellidos'];
+            $this -> id = $fila['id'];
+            $this -> nombre = $fila['nombre'];
             $this -> email = $fila['email'];
         }
 
 
         public function Eliminar(){
             $sql = "DELETE FROM Usuarios 
-                WHERE idUsuario = " . $this -> idUsuario . ";";
+                WHERE id = " . $this -> id . ";";
             $this -> conexion -> query($sql);
         }
 
@@ -64,9 +60,8 @@
             $resultado = array();
             foreach($filas as $fila){
                 $p = new UsuariosModelo();
-                $p -> idUsuario  = $fila['idUsuario'];
-                $p -> nombres = $fila['nombres'];
-                $p -> apellidos = $fila['apellidos'];
+                $p -> id  = $fila['id'];
+                $p -> nombre = $fila['nombre'];
                 $p -> email = $fila['email'];
 
                 array_push($resultado,$p);
